c689320748f86b50cb95cbb1d7db958e0518001a
Guilherme <guilherme.kaliter@gmail.com>
2023-03-05 12:18:30 -0300

    feat: updated resume

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,58 +1,58 @@
-(function() {
+(function () {
 	'use strict';
 
 	/*----------------------------------------
 		Detect Mobile
 	----------------------------------------*/
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
 
 	// navigation
-	var OnePageNav = function() {
+	var OnePageNav = function () {
 		var navToggler = $('.navbar-toggler');
-		$(".smoothscroll[href^='#'], #pb-navbar ul li a[href^='#']").on('click', function(e) {
-		 	e.preventDefault();
-		 	var hash = this.hash;
-		 		
-		 	$('html, body').animate({
+		$(".smoothscroll[href^='#'], #pb-navbar ul li a[href^='#']").on('click', function (e) {
+			e.preventDefault();
+			var hash = this.hash;
 
-		    scrollTop: $(hash).offset().top
-		  }, 700, 'easeInOutExpo', function(){
-		    window.location.hash = hash;
-		  });
+			$('html, body').animate({
+
+				scrollTop: $(hash).offset().top
+			}, 700, 'easeInOutExpo', function () {
+				window.location.hash = hash;
+			});
 		});
-		$("#pb-navbar ul li a[href^='#']").on('click', function(e){
-			if ( navToggler.is(':visible') ) {
-		  	navToggler.click();
-		  }
+		$("#pb-navbar ul li a[href^='#']").on('click', function (e) {
+			if (navToggler.is(':visible')) {
+				navToggler.click();
+			}
 		});
 
 		$('body').on('activate.bs.scrollspy', function () {
-		  console.log('nice');
+			console.log('nice');
 		})
 	};
-	
 
-	var offCanvasNav = function() {
+
+	var offCanvasNav = function () {
 		// var toggleNav = $('.js-pb_nav-toggle'),
 		// 		offcanvasNav = $('.js-pb_offcanvas-nav_v1');
 		// if( toggleNav.length > 0 ) {
@@ -70,179 +70,179 @@
 		// 	e.preventDefault();
 		// })
 	};
-	
+
 
 
 	/*----------------------------------------
 		Animate Scroll
 	----------------------------------------*/
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.site-animate').waypoint( function( direction ) {
+		$('.site-animate').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('site-animated') ) {
-				
+			if (direction === 'down' && !$(this.element).hasClass('site-animated')) {
+
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .site-animate.item-animate').each(function(k){
+					$('body .site-animate.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn site-animated');
-							} else if ( effect === 'fadeInLeft') {
+							} else if (effect === 'fadeInLeft') {
 								el.addClass('fadeInLeft site-animated');
-							} else if ( effect === 'fadeInRight') {
+							} else if (effect === 'fadeInRight') {
 								el.addClass('fadeInRight site-animated');
 							} else {
 								el.addClass('fadeInUp site-animated');
 							}
 							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
+						}, k * 100, 'easeInOutExpo');
 					});
-					
+
 				}, 100);
-				
+
 			}
 
-		} , { offset: '95%' } );
+		}, { offset: '95%' });
 	};
 
-	var navbarState = function() {
+	var navbarState = function () {
 
 		var lastScrollTop = 0;
-		$(window).scroll(function(){
+		$(window).scroll(function () {
 
 			var $this = $(this),
-				 	st = $this.scrollTop(),
-				 	navbar = $('.site-navbar');
+				st = $this.scrollTop(),
+				navbar = $('.site-navbar');
 
-			if ( st > 200 ) {
+			if (st > 200) {
 				navbar.addClass('scrolled');
 			} else {
 				navbar.removeClass('scrolled awake');
 			}
 
-			if ( navbar.hasClass('scrolled') && st > 300 ) {
-		   	if (st > lastScrollTop){
-		      // if (navbar.hasClass('scrolled')) {
-		      	// navbar.removeClass('awake');
-		      	// navbar.addClass('sleep');
-		      // }
-		   	} else {
-		      // if (navbar.hasClass('scrolled')) {
-		      	// navbar.addClass('awake');
-		      	// navbar.removeClass('sleep');
-		      // }
-		   	}
-		   	lastScrollTop = st;
-		  }
+			if (navbar.hasClass('scrolled') && st > 300) {
+				if (st > lastScrollTop) {
+					// if (navbar.hasClass('scrolled')) {
+					// navbar.removeClass('awake');
+					// navbar.addClass('sleep');
+					// }
+				} else {
+					// if (navbar.hasClass('scrolled')) {
+					// navbar.addClass('awake');
+					// navbar.removeClass('sleep');
+					// }
+				}
+				lastScrollTop = st;
+			}
 
 		});
 
 
 
-		
+
 	};
 
-	
-	
-	
-	var siteStellar = function() {
+
+
+
+	var siteStellar = function () {
 		$(window).stellar({
-	    responsive: true,
-	    parallaxBackgrounds: true,
-	    parallaxElements: true,
-	    horizontalScrolling: false,
-	    hideDistantElements: false,
-	    scrollProperty: 'scroll'
-	  });
+			responsive: true,
+			parallaxBackgrounds: true,
+			parallaxElements: true,
+			horizontalScrolling: false,
+			hideDistantElements: false,
+			scrollProperty: 'scroll'
+		});
 	};
-	
+
 
 
 
 	// Page Nav
-	var clickMenu = function() {
+	var clickMenu = function () {
 
-		$('.navbar-nav a:not([class="external"])').click(function(event){
+		$('.navbar-nav a:not([class="external"])').click(function (event) {
 
 			var section = $(this).data('nav-section'),
 				navbar = $('.navbar-nav');
-				if (isMobile.any()) {
-					$('.navbar-toggle').click();
-				}
-				if ( $('[data-section="' + section + '"]').length ) {
-			    	$('html, body').animate({
-			        	scrollTop: $('[data-section="' + section + '"]').offset().top
-			    	}, 500, 'easeInOutExpo');
-			   }
+			if (isMobile.any()) {
+				$('.navbar-toggle').click();
+			}
+			if ($('[data-section="' + section + '"]').length) {
+				$('html, body').animate({
+					scrollTop: $('[data-section="' + section + '"]').offset().top
+				}, 500, 'easeInOutExpo');
+			}
 
-		    event.preventDefault();
-		    return false;
+			event.preventDefault();
+			return false;
 		});
 
 
 	};
 
 	// Reflect scrolling in navigation
-	var navActive = function(section) {
+	var navActive = function (section) {
 
 		var $el = $('.navbar-nav');
 		$el.find('li').removeClass('active');
-		$el.each(function(){
-			$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
+		$el.each(function () {
+			$(this).find('a[data-nav-section="' + section + '"]').closest('li').addClass('active');
 		});
 
 	};
 
-	var navigationSection = function() {
+	var navigationSection = function () {
 
 		var $section = $('section[data-section]');
-		
-		$section.waypoint(function(direction) {
-		  	if (direction === 'down') {
-		    	navActive($(this.element).data('section'));
-		  	}
+
+		$section.waypoint(function (direction) {
+			if (direction === 'down') {
+				navActive($(this.element).data('section'));
+			}
 		}, {
-	  		offset: '150px'
+			offset: '150px'
 		});
 
-		$section.waypoint(function(direction) {
-		  	if (direction === 'up') {
-		    	navActive($(this.element).data('section'));
-		  	}
+		$section.waypoint(function (direction) {
+			if (direction === 'up') {
+				navActive($(this.element).data('section'));
+			}
 		}, {
-		  	offset: function() { return -$(this.element).height() - 155; }
+			offset: function () { return -$(this.element).height() - 155; }
 		});
 
 	};
 
 
-	var smoothScroll = function() {
+	var smoothScroll = function () {
 		var $root = $('html, body');
 
 		$('.smoothscroll').click(function () {
 			$root.animate({
-		    scrollTop: $( $.attr(this, 'href') ).offset().top
+				scrollTop: $($.attr(this, 'href')).offset().top
 			}, 500);
 			return false;
 		});
 	};
-	
-	var magnificPopupControl = function() {
+
+	var magnificPopupControl = function () {
 
 
 		$('.image-popup').magnificPopup({
 			type: 'image',
 			removalDelay: 300,
 			mainClass: 'mfp-with-zoom',
-			gallery:{
-				enabled:true
+			gallery: {
+				enabled: true
 			},
 			zoom: {
 				enabled: true, // By default it's false, so don't forget to enable it
@@ -253,10 +253,10 @@
 				// The "opener" function should return the element from which popup will be zoomed in
 				// and to which popup will be scaled down
 				// By defailt it looks for an image tag:
-				opener: function(openerElement) {
-				// openerElement is the element on which popup was initialized, in this case its <a> tag
-				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
-				return openerElement.is('img') ? openerElement : openerElement.find('img');
+				opener: function (openerElement) {
+					// openerElement is the element on which popup was initialized, in this case its <a> tag
+					// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+					return openerElement.is('img') ? openerElement : openerElement.find('img');
 				}
 			}
 		});
@@ -268,8 +268,8 @@
 			mainClass: 'mfp-with-zoom mfp-img-mobile',
 			image: {
 				verticalFit: true,
-				titleSrc: function(item) {
-					return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+				titleSrc: function (item) {
+					return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
 				}
 			},
 			zoom: {
@@ -279,47 +279,56 @@
 
 
 		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-      disableOn: 700,
-      type: 'iframe',
-      mainClass: 'mfp-fade',
-      removalDelay: 160,
-      preloader: false,
+			disableOn: 700,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
 
-      fixedContentPos: false
-    });
+			fixedContentPos: false
+		});
 	};
 
 
 
 
-	var portfolioMasonry = function() {
- $('.filters ul li').click(function(){
-        $('.filters ul li').removeClass('active');
-        $(this).addClass('active');
-        
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-          filter: data
-        })
-      });
+	var portfolioMasonry = function () {
+		$('.filters ul li').click(function () {
+			$('.filters ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var data = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: data
+			})
+		});
 
 
-      if(document.getElementById("section-portfolio")){
-            var $grid = $(".grid").isotope({
-              itemSelector: ".all",
-              percentPosition: true,
-              masonry: {
-                columnWidth: ".all"
-              }
-            })
-      };
+		if (document.getElementById("section-portfolio")) {
+			var $grid = $(".grid").isotope({
+				itemSelector: ".all",
+				percentPosition: true,
+				masonry: {
+					columnWidth: ".all"
+				}
+			})
+		};
 
 
 	};
+	var i = 0;
+	var txt = "Guilherme Xavier"
+	var speed = 100; /* The speed/duration of the effect in milliseconds */
+	function typeWriter() {
+		if (i < txt.length) {
+			document.getElementsByClassName("my-name")[0].innerHTML += txt.charAt(i);
+			i++;
+			setTimeout(typeWriter, speed);
+		}
+	}
 
-
-	$(function(){
-
+	$(function () {
+		typeWriter()
 		OnePageNav();
 		offCanvasNav();
 		contentWayPoint();
@@ -329,7 +338,7 @@
 		portfolioMasonry();
 	});
 
-	
+
 
 
 })();
